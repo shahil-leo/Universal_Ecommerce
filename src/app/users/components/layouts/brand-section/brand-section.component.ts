c88799649602158a91d810912677da1bae8a4fdb
shahil <codeshahil@gmail.com>
2023-10-01 16:59:48 +0530

    added the brand section for applications allover

---
+++ src/app/users/components/layouts/brand-section/brand-section.component.ts
@@ -0,0 +1,47 @@
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+interface brandListModal{
+  font:string,
+  brandName:string
+}
+
+@Component({
+  selector: 'app-brand-section',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './brand-section.component.html',
+  styleUrls: ['./brand-section.component.scss']
+})
+export class BrandSectionComponent implements OnInit {
+
+brandList:brandListModal[]=[]
+
+  constructor(){}
+
+  ngOnInit(): void {
+      this.brandList=[
+
+      {
+        font:'',
+        brandName:'Nike'
+      },
+      {
+        font:'',
+        brandName:'Gucci'
+      },
+      {
+        font:'',
+        brandName:'Puma'
+      },
+      {
+        font:'',
+        brandName:'Adidas'
+      },
+
+
+    ]
+  }
+  getBrandListProducts(brandName:string){
+  }
+}
--- src/app/users/components/pages/home/home.component.ts
+++ src/app/users/components/pages/home/home.component.ts
@@ -2,12 +2,13 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NavbarComponent } from '../../layouts/navbar/navbar.component';
 import { CarouselHeroComponent } from '../../layouts/carousel-hero/carousel-hero.component';
-CarouselHeroComponent
+import { BrandSectionComponent } from '../../layouts/brand-section/brand-section.component';
+
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule,NavbarComponent,CarouselHeroComponent],
+  imports: [CommonModule,NavbarComponent,CarouselHeroComponent,BrandSectionComponent],
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
