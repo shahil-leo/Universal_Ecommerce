b2d54b74d5293be0ae38fc65909406653f83d604
shahil <codeshahil@gmail.com>
2023-10-02 18:41:20 +0530

    added the show-case component to show users what things are there

---
+++ src/app/users/components/layouts/show-case/show-case.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-show-case',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './show-case.component.html',
+  styleUrls: ['./show-case.component.scss']
+})
+export class ShowCaseComponent {
+
+}
--- src/app/users/components/pages/home/home.component.ts
+++ src/app/users/components/pages/home/home.component.ts
@@ -3,12 +3,12 @@ import { CommonModule } from '@angular/common';
 import { NavbarComponent } from '../../layouts/navbar/navbar.component';
 import { CarouselHeroComponent } from '../../layouts/carousel-hero/carousel-hero.component';
 import { BrandSectionComponent } from '../../layouts/brand-section/brand-section.component';
-
+import { ShowCaseComponent } from '../../layouts/show-case/show-case.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule,NavbarComponent,CarouselHeroComponent,BrandSectionComponent],
+  imports: [CommonModule,NavbarComponent,CarouselHeroComponent,BrandSectionComponent,ShowCaseComponent],
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
